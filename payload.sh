5a1218599b8407827b46083e24554547a40fa8ba
Mateo Estrada Ramírez <mateoestradaramirez@outlook.com>
2024-01-06 23:33:31 -0500

    Add payload

---
+++ payload.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+clear
+echo "Installing git..."
+
+sleep 2
+
+pkg update && pkg install git -y
+
+clear
+echo "Cloning repository"
+
+sleep 2
+
+git clone https://github.com/dpkg-i-foo-deb/termux-xfce.git
+
+cd termux-xfce || exit
+
+./main.sh
