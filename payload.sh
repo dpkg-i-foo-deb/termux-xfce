10f5187ead886c0023aa154f0a1d892d946fe2ea
Mateo Estrada Ramírez <mateoestradaramirez@outlook.com>
2024-01-07 19:22:27 -0500

    Messages

--- payload.sh
+++ payload.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+clear
 echo "Updating packages"
 sleep 5
 
 pkg update -y -o Dpkg::Options::="--force-confold"
 pkg upgrade -y -o Dpkg::Options::="--force-confold"
 
+clear
 echo "Updating repositories"
+sleep 5
+
 termux-change-repo
 
 clear
@@ -30,4 +34,19 @@ cd termux-xfce || exit
 
 cd ..
 
+clear
+echo "Deleting repository files"
+sleep 5
+
 rm -rf termux-xfce
+
+clear
+echo "Done! XFCE should be installed on Termux as well as Termux-X11"
+echo "and Debian is configured using proot-distro, restart the Termux app"
+echo "or source .bashrc to get started"
+echo ""
+echo "You can start the graphical environment running start-de and stop it using stop-de"
+echo "It should bring up XFCE on Termux-X11"
+echo ""
+echo "You can enter the proot environment running debian and it will log you in with the user"
+echo "you specified during the install"
